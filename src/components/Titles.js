ee5a6600d6214e3bc11146570c38f49e10f1f23e
ahochman <ahochman2019@gmail.com>
2019-06-26 15:33:25 -0400

    styling and such

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Titles = () => (
-  <React.Fragment>
-    <h1>Weather Finder</h1>
-    <p>Find out temperature, conditions and more</p>
-  </React.Fragment>
+  <div>
+    <h1 className="title-container__title">Weather Finder</h1>
+    <p className="title-container__subtitle">Find out temperature, conditions and more</p>
+  </div>
 );
 
 export default Titles;
