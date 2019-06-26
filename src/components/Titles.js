47172feab2815e8a35e000c85669a9f01f783a0b
ahochman <ahochman2019@gmail.com>
2019-06-26 14:55:25 -0400

    stateless components

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,10 @@
 import React from "react";
 
-class Titles extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Weather Finder</h1>
-        <p>Find out temperature, conditions and more</p>
-        </React.Fragment>
-    );
-  }
-};
+const Titles = () => (
+  <React.Fragment>
+    <h1>Weather Finder</h1>
+    <p>Find out temperature, conditions and more</p>
+  </React.Fragment>
+);
 
 export default Titles;
